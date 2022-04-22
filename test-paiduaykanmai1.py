e115d52c8b8128b8018294d5a6e65f114561ee94
Icesolo <jmfairy123@hotmail.com>
2022-04-22 16:58:32 +0700

    new update

--- test-paiduaykanmai1.py
+++ test-paiduaykanmai1.py
@@ -1,11 +1,16 @@
 number = int(input());
-codes = str(input());
+codes = str(input()); #RRB
 
-tmp = "";   
+tmp = "";   #R G B
 count = 0;
 for code in codes:
     if tmp == code:
         count += 1;
     tmp = code;
+    # print(len(codes))
+    if len(codes) > number:
+        print("จำนวนเกิน")
+        break
 
-print("Output : ", count )
+
+print("Output : ", count + "คู่")
